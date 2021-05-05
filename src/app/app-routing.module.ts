abc25ba89633a3aecef36919cd82f876ef1cacba
Nora-92 <noravallo1992@gmail.com>
2021-05-05 10:45:43 +0200

    route creation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { JobsDetailComponent } from './jobs-detail/jobs-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent},
+  { path: 'jobs/:id', component: JobsDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
